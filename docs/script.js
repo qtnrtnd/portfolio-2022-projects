9f057f4f600e095ec633431d6852dbf7077075cc
Quentin Ratinaud <contact@falld.studio>
2022-03-30 22:15:10 +0200

    Header + animations v2, footer, responsive v1

--- docs/script.js
+++ docs/script.js
@@ -1,24 +1,56 @@
 const ol = document.querySelector('ol');
 const cursor = document.querySelector('.cursor');
 
+/*const maxViewportHeight = Math.max(window.screen.availHeight - (window.outerHeight - window.innerHeight), window.innerHeight);
+document.body.style.setProperty('--doc-min-height', maxViewportHeight + "px");*/
+
 document.querySelectorAll('li').forEach(li => {
 
-    li.addEventListener('mouseenter', function () {
+    const dataThumbnail = li.getAttribute('data-thumbnail');
+    const thumbnail = document.querySelector(".project-thumbnail[data-thumbnail='" + dataThumbnail + "']");
+    //const rightSide = dataThumbnail % 2 === 0;
+
+    const moveHandler = function (e, that) {
+
+        let elt = that ? that : this;
+
+        let x = (e.offsetX - elt.offsetWidth / 2) / 24;
+        let y = (e.offsetY - elt.offsetHeight / 2) / 4;
+
+        thumbnail.style.transform = `translate3d(${-x}px, ${-y}px, 0)`;
+    }
+
+    li.addEventListener('mouseenter', function (e) {
+
+        thumbnail.style.transition = "none";
+
+        moveHandler(e, this);
+
+        thumbnail.offsetHeight;
+
+        thumbnail.style.removeProperty("transition");
+
+        li.addEventListener('mousemove', moveHandler);
+
+        //if (rightSide) {
+            thumbnail.style.left = this.offsetLeft + this.offsetWidth + "px";
+        /*} else {
+            thumbnail.style.left = this.offsetLeft - thumbnail.offsetWidth + "px";
+        }*/
+
+        thumbnail.style.top = this.offsetTop + this.offsetHeight / 2 - thumbnail.offsetHeight / 2  + "px";
+
         ol.classList.add('hovering');
+        thumbnail.classList.add('visible');
+
     });
 
     li.addEventListener('mouseleave', function () {
+
+        li.removeEventListener('mousemove', moveHandler);
         ol.classList.remove('hovering');
+        thumbnail.classList.remove('visible');
 
-    });
-
-    li.addEventListener('mousemove', function (e) {
-        let x = (e.offsetX - this.offsetWidth / 2) / 16;
-        let y = (e.offsetY - this.offsetHeight / 2) / 16;
-
-        let thumbnail = this.querySelector('.project-thumbnail');
-
-        thumbnail.style.transform = `translate3d(${-x}px, ${-y}px, 0)`;
     });
 
 });
